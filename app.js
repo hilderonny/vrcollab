20356f0a849b67be9654300ee52f70ba14596fc0
rh <ronny.hildebrandt@avorium.de>
2017-10-24 15:07:08 +0200

    Application created which renders websites as images.

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+app.use('/api/phantom', require('./api/phantom'));
+
+const HTTP_PORT = 80;
+const HTTPS_PORT = 443;
+
+var server = https.createServer({ 
+    key: fs.readFileSync('./priv.key', 'utf8'), 
+    cert: fs.readFileSync('./pub.cert', 'utf8')
+}, app);
+
+server.listen(HTTPS_PORT, () => {
+    console.log(`HTTPS running at port ${HTTPS_PORT}.`);
+});
+http.createServer((req, res) => {
+    var indexOfColon = req.headers.host.lastIndexOf(':');
+    var hostWithoutPort = indexOfColon > 0 ? req.headers.host.substring(0, indexOfColon) : req.headers.host;
+    var newUrl = `https://${hostWithoutPort}:${HTTPS_PORT}${req.url}`;
+    res.writeHead(302, { 'Location': newUrl });
+    res.end();
+}).listen(HTTP_PORT, function() {
+    console.log(`HTTP running at port ${HTTP_PORT}.`);
+});
