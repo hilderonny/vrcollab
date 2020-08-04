6c958819e7b3c3c28aa53057c2cc8b76f964c2b4
root <root@localhost.localdomain>
2020-08-04 10:54:24 +0000

    Mauskamere geht jetzt besser

--- public/js/controls.js
+++ public/js/controls.js
@@ -1,26 +1,37 @@
 import camera from './camera.js';
 
+var mouseSpeed = 2;
+
 var controls = {
 
     init: function (renderer) {
-        var startX, startY;
+        var startX, startY, mouseDown;
         var mouseMove = function(e) {
-            var deltaX = e.clientX - startX;
-            var deltaY = e.clientY - startY;
-            var width = window.innerWidth, height = window.innerHeight, min = Math.min(width, height);
-            camera.cam3.rotation.x += deltaY/min;
-            camera.cam3.rotation.y += deltaX/min;
-            startX = e.clientX; startY = e.clientY;
+            if (mouseDown) {
+                var deltaX = e.clientX - startX;
+                var deltaY = e.clientY - startY;
+                var width = window.innerWidth, height = window.innerHeight, min = Math.min(width, height);
+                camera.cam3.rotation.x += deltaY/min * mouseSpeed;
+                camera.head.rotation.y += deltaX/min * mouseSpeed;
+                startX = e.clientX; startY = e.clientY;
+            }
         };
-        renderer.domElement.addEventListener('mousedown', function(e) {
-            renderer.domElement.addEventListener('mousemove', mouseMove);
-            startX = e.clientX; startY = e.clientY;
-            e.preventDefault();
-		    e.stopPropagation();
+        renderer.domElement.addEventListener('mousedown', function(event) {
+            // React only to right mouse button for movement
+            if (event.button == 2) {
+                startX = event.clientX; startY = event.clientY;
+                mouseDown = true;
+            }
         });
-        renderer.domElement.addEventListener('mouseup', function(e) {
-            renderer.domElement.removeEventListener('mousemove', mouseMove);
+        renderer.domElement.addEventListener('mouseup', function() {
+            if (event.button == 2) {
+                mouseDown = false;
+            }
         });
+        renderer.domElement.addEventListener('contextmenu', function(event) {
+            event.preventDefault();
+        });
+        renderer.domElement.addEventListener('mousemove', mouseMove);
     },
 
 };
